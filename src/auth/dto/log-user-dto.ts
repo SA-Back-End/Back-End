d015d25a91bec4007c1e2fa809df2fa98c6208ae
João Vitor Perfeito <jvperfeito@gmail.com>
2023-08-14 14:58:46 -0300

    Fix: auth login

--- src/auth/dto/log-user-dto.ts
+++ src/auth/dto/log-user-dto.ts
@@ -1,4 +1,14 @@
+import { ApiProperty } from "@nestjs/swagger";
+import { IsNotEmpty, IsString } from "class-validator";
+
 export class LogUserDto {
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({ type: String, description: 'Username do usuário'  })
     username: string;
+
+    @IsString()
+    @IsNotEmpty()
+    @ApiProperty({ type: String, description: 'Senha do usuário'  })
     password: string;
 }
