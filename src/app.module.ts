b556455bcf3771525a3219e7e35526d11bdcd1b1
João Vitor Perfeito <jvperfeito@gmail.com>
2023-08-21 16:46:41 -0300

    fix: module routes

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,11 @@ import { PostModule } from './modules/post/post.module';
 import { ProjectModule } from './modules/project/project.module';
 import { PrismaService } from './database/PrismaService';
 import { AuthModule } from './auth/auth.module';
-import { FormationModule } from './formation/formation.module';
-import { InstitutionModule } from './institution/institution.module';
-import { ExperienceModule } from './experience/experience.module';
-import { CertificatesModule } from './certificates/certificates.module';
-import { ScreenStickModule } from './screen_stick/screen_stick.module';
+import { FormationModule } from './modules/formation/formation.module';
+import { InstitutionModule } from './modules/institution/institution.module';
+import { ExperienceModule } from './modules/experience/experience.module';
+import { CertificatesModule } from './modules/certificates/certificates.module';
+import { ScreenStickModule } from './modules/screen_stick/screen_stick.module';
 
 @Module({
   imports: [UserModule, PostModule, ProjectModule, AuthModule, FormationModule, InstitutionModule, ExperienceModule, CertificatesModule, ScreenStickModule],
