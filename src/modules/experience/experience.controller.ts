be3d292bfab1e4fee21ceb1945a105c0cb60b078
CodexCris <cristian_moraes-j@estudante.sesiseani.org.br>
2023-08-28 15:20:54 -0300

    controller

--- src/modules/experience/experience.controller.ts
+++ src/modules/experience/experience.controller.ts
@@ -2,32 +2,51 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { ExperienceService } from './experience.service';
 import { CreateExperienceDto } from './dto/create-experience.dto';
 import { UpdateExperienceDto } from './dto/update-experience.dto';
+import { ApiBadRequestResponse, ApiConflictResponse, ApiCreatedResponse, ApiNotAcceptableResponse, ApiNotFoundResponse, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @Controller('experience')
 export class ExperienceController {
   constructor(private readonly experienceService: ExperienceService) {}
 
   @Post()
+  @ApiCreatedResponse({ description: 'Experiência criada com sucesso', type: CreateExperienceDto, status: 201 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiNotAcceptableResponse({ description: 'Experiência ou senha muito pequenas', status: 406 })
+  @ApiConflictResponse({ description: 'Experiência já existente!', status: 409 })
   create(@Body() createExperienceDto: CreateExperienceDto) {
     return this.experienceService.create(createExperienceDto);
   }
 
   @Get()
+  @ApiOkResponse({ description: 'Informações encontradas', type: CreateExperienceDto, status: 200 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiUnauthorizedResponse({ description: 'Acesso não autorizado', status: 401 })
   findAll() {
     return this.experienceService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'Informações encontradas', type: CreateExperienceDto, status: 200 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiUnauthorizedResponse({ description: 'Acesso não autorizado', status: 401 })
   findOne(@Param('id') id: string) {
     return this.experienceService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ description: 'Informações editadas com sucesso', type: UpdateExperienceDto, status: 200 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiUnauthorizedResponse({ description: 'Acesso não autorizado', status: 401 })
+  @ApiNotFoundResponse({ description: 'Experiência não existente', status: 404 })
   update(@Param('id') id: string, @Body() updateExperienceDto: UpdateExperienceDto) {
     return this.experienceService.update(+id, updateExperienceDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: 'Experiência deletada com sucesso', status: 200 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiUnauthorizedResponse({ description: 'Acesso não autorizado', status: 401 })
+  @ApiNotFoundResponse({ description: 'Experiência não existente', status: 404 })
   remove(@Param('id') id: string) {
     return this.experienceService.remove(+id);
   }
--- src/modules/formation/formation.controller.ts
+++ src/modules/formation/formation.controller.ts
@@ -2,32 +2,51 @@ import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/commo
 import { FormationService } from './formation.service';
 import { CreateFormationDto } from './dto/create-formation.dto';
 import { UpdateFormationDto } from './dto/update-formation.dto';
+import { ApiBadRequestResponse, ApiConflictResponse, ApiCreatedResponse, ApiNotAcceptableResponse, ApiNotFoundResponse, ApiOkResponse, ApiUnauthorizedResponse } from '@nestjs/swagger';
 
 @Controller('formation')
 export class FormationController {
   constructor(private readonly formationService: FormationService) {}
 
   @Post()
+  @ApiCreatedResponse({ description: 'Formação criado com sucesso', type: CreateFormationDto, status: 201 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiNotAcceptableResponse({ description: 'Formação ou senha muito pequenos', status: 406 })
+  @ApiConflictResponse({ description: 'Formação já existente!', status: 409 })
   create(@Body() createFormationDto: CreateFormationDto) {
     return this.formationService.create(createFormationDto);
   }
 
   @Get()
+  @ApiOkResponse({ description: 'Informações encontradas', type: CreateFormationDto, status: 200 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiUnauthorizedResponse({ description: 'Acesso não autorizado', status: 401 })
   findAll() {
     return this.formationService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ description: 'Informações encontradas', type: CreateFormationDto, status: 200 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiUnauthorizedResponse({ description: 'Acesso não autorizado', status: 401 })
   findOne(@Param('id') id: string) {
     return this.formationService.findOne(+id);
   }
 
   @Patch(':id')
+  @ApiOkResponse({ description: 'Informações editadas com sucesso', type: UpdateFormationDto, status: 200 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiUnauthorizedResponse({ description: 'Acesso não autorizado', status: 401 })
+  @ApiNotFoundResponse({ description: 'Formação não existente', status: 404 })
   update(@Param('id') id: string, @Body() updateFormationDto: UpdateFormationDto) {
     return this.formationService.update(+id, updateFormationDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ description: 'Formação deletado com sucesso', status: 200 })
+  @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
+  @ApiUnauthorizedResponse({ description: 'Acesso não autorizado', status: 401 })
+  @ApiNotFoundResponse({ description: 'Formação não existente', status: 404 })
   remove(@Param('id') id: string) {
     return this.formationService.remove(+id);
   }
