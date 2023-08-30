3fc9dda0cd0257fe2af16e7830c6a0aee51d4668
Vinicius <vinicius.dla2106@gmail.com>
2023-08-30 16:22:11 -0300

    fix: audit studyArea and begin date updt

--- src/modules/experience/dto/create-experience.dto.ts
+++ src/modules/experience/dto/create-experience.dto.ts
@@ -1,6 +1,6 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { StudyArea } from '@prisma/client';
-import { IsNotEmpty, IsNumber, IsOptional, IsString, IsDate, IsArray, IsDateStringString, IsArray } from 'class-validator';
+import { IsNotEmpty, IsNumber, IsOptional, IsString, IsDateString, IsArray} from 'class-validator';
 
 export class CreateExperienceDto {
 
@@ -15,8 +15,8 @@ export class CreateExperienceDto {
   institutionId: number 
 
 	@IsNotEmpty()
-	@IsString()
-	@ApiProperty({ type: String, description: 'A área de estudo da experiência', required: true  })
+	@IsArray()
+	@ApiProperty({ type: [String], description: 'A área de estudo da experiência', required: true  })
   studyArea: StudyArea[]
 
 	@IsNotEmpty()
