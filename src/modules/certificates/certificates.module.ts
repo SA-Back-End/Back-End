5a2b86665e41fd6d8773b74b9dd7dba4058b8f31
João Vitor Perfeito <jvperfeito@gmail.com>
2023-08-30 14:51:52 -0300

    fix: data errors certificates crud

--- src/modules/certificates/certificates.module.ts
+++ src/modules/certificates/certificates.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CertificatesService } from './certificates.service';
 import { CertificatesController } from './certificates.controller';
+import { PrismaService } from 'src/database/PrismaService';
 
 @Module({
   controllers: [CertificatesController],
-  providers: [CertificatesService],
+  providers: [CertificatesService, PrismaService],
 })
 export class CertificatesModule {}
