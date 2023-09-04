f62cfba690377154c5752a0b290d5a3adb2616e0
Vinicius <vinicius.dla2106@gmail.com>
2023-09-04 14:33:49 -0300

    fix: add needed properts to update

--- src/modules/user/dto/update-user.dto.ts
+++ src/modules/user/dto/update-user.dto.ts
@@ -1,4 +1,22 @@
 import { PartialType } from '@nestjs/mapped-types';
+import { ApiProperty } from '@nestjs/swagger';
+import { HardSkills, SoftSkills, StudyArea } from '@prisma/client';
+import { IsArray, IsOptional } from 'class-validator';
 import { CreateUserDto } from './create-user.dto';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @ApiProperty({ type: String, description: 'HardSkills', example: ['Desenvolvimento_web']})
+    @IsOptional()
+    @IsArray()
+    hardSkills?: HardSkills[]
+
+    @ApiProperty({ type: String, description: 'SoftSkills', example: ['Trabalho_em_equipe']})
+    @IsOptional()
+    @IsArray()
+    softSkills?: SoftSkills[]
+
+    @ApiProperty({ type: String, description: 'StudyArea', example: ['Ciencia_da_Computacao']})
+    @IsOptional()
+    @IsArray()
+    studyarea?: StudyArea[]
+}
