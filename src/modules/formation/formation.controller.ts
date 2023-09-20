3326dfc39a86b27a41d375e85933d28025e25219
Vini <viniciusicmsc@gmail.com>
2023-09-20 15:13:14 -0300

    2.0

--- src/modules/formation/formation.controller.ts
+++ src/modules/formation/formation.controller.ts
@@ -15,7 +15,7 @@ export class FormationController {
   @ApiCreatedResponse({ description: 'Formação criada com sucesso', type: CreateFormationDto, status: 201 })
   @ApiBadRequestResponse({ description: 'Requisição inválida', status: 400 })
   @ApiNotAcceptableResponse({ description: 'Nome da Formação ou senha muito pequenos', status: 406 })
-  @ApiConflictResponse({ description: 'Formação já existente!', status: 409 })
+  @ApiConflictResponse({ description: 'Formação já existente', status: 409 })
   @ApiOperation({
     summary: 'Cria uma formação',
     description: 'Cria uma formação na plataforma',
