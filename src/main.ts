a598a082f92d8323eeabaf1aebf1cc4782ff8697
Vinicius de Lima Albino <70918792+vini-se@users.noreply.github.com>
2023-09-27 10:51:41 -0300

    fix: enable cors
    React (http://127.0.0.1.3001) couldn't reach node (http://127.0.0.1:3000) by CORS politics

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
 
   app.useGlobalPipes(
     new ValidationPipe({
