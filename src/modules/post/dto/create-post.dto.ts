44d02454505b8757ec8c0635fabf65dfafc1f305
João Vitor Perfeito <jvperfeito@gmail.com>
2023-09-27 16:57:11 -0300

    feat: 3 imgs max limit in a post

--- src/modules/post/dto/create-post.dto.ts
+++ src/modules/post/dto/create-post.dto.ts
@@ -2,22 +2,40 @@ import { ApiProperty } from '@nestjs/swagger';
 import { User } from '@prisma/client';
 //import { HardSkills } from '@prisma/client';
 import { Type } from 'class-transformer';
-import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
-
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreatePostDto {
-    @ApiProperty({ type: Number, description: 'Id do autor da postagem', example: 45 })
-    @IsNumber()
-    @IsNotEmpty()
-    userId: number;
+  @ApiProperty({
+    type: Number,
+    description: 'Id do autor da postagem',
+    example: 45,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  userId: number;
 
-    @ApiProperty({ type: String, description: 'Texto da postagem', example: 'lorem ipsun ipsun lorem' })
-    @IsString()
-    @IsNotEmpty()
-    text: string;
+  @ApiProperty({
+    type: String,
+    description: 'Texto da postagem',
+    example: 'lorem ipsun ipsun lorem',
+  })
+  @IsString()
+  @IsNotEmpty()
+  text: string;
 
-    @ApiProperty({ type: String,format:'byte', description: 'Imagem da postagem', example: 'U3dhZ2dlciByb2Nrcw==' })
-    @IsString()
-    @IsNotEmpty()
-    post_img: Buffer;
+  @ApiProperty({
+    type: [String],
+    format: 'byte',
+    description: 'Imagem da postagem',
+    example: ['U3dhZ2dlciByb2Nrcw=='],
+  })
+  @IsArray()
+  @IsNotEmpty()
+  post_img: Buffer[];
 }
