ecdb4f86f1479e98901a79ae8f03c1b973115380
João Vitor Perfeito <jvperfeito@gmail.com>
2023-10-11 16:47:17 -0300

    fix: description of study area

--- src/modules/project/dto/create-project.dto.ts
+++ src/modules/project/dto/create-project.dto.ts
@@ -2,39 +2,73 @@ import { ApiProperty } from '@nestjs/swagger';
 import { StatusProject, StudyArea, WorkType } from '@prisma/client';
 //import { HardSkills } from '@prisma/client';
 import { Type } from 'class-transformer';
-import { IsArray, IsNotEmpty, IsNumber, IsOptional, IsString } from 'class-validator';
+import {
+  IsArray,
+  IsNotEmpty,
+  IsNumber,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class CreateProjectDto {
-    @ApiProperty({type: Number, description: 'ID do usuário dono do projeto', example: 33})
-    @IsNumber()
-    @IsNotEmpty()
-    id_projectManager: number;
+  @ApiProperty({
+    type: Number,
+    description: 'ID do usuário dono do projeto',
+    example: 33,
+  })
+  @IsNumber()
+  @IsNotEmpty()
+  id_projectManager: number;
 
-    @ApiProperty({ type: String, description: 'Status do projeto', example: 'Cancelado' })
-    @IsNotEmpty()
-    status: StatusProject;
+  @ApiProperty({
+    type: String,
+    description: 'Status do projeto',
+    example: 'Cancelado',
+  })
+  @IsNotEmpty()
+  status: StatusProject;
 
-    @ApiProperty({ type: [String], description: 'Areas pesquisadas pelo projeto', example: ["Medicina"] })
-    @IsArray()
-    @IsNotEmpty()
-    studyArea: StudyArea[];
+  @ApiProperty({
+    type: [String],
+    description: 'Areas pesquisadas pelo projeto',
+    example: ['Ciencias_Exatas_e_da_Terra'],
+  })
+  @IsArray()
+  @IsNotEmpty()
+  studyArea: StudyArea[];
 
-    @ApiProperty({ type: String, description: 'Título do projeto', example: 'Titulo' })
-    @IsString()
-    @IsNotEmpty()
-    project_name: string;
+  @ApiProperty({
+    type: String,
+    description: 'Título do projeto',
+    example: 'Titulo',
+  })
+  @IsString()
+  @IsNotEmpty()
+  project_name: string;
 
-    @ApiProperty({ type: String, description: 'Descrição do projeto', example: 'Lorem ipsum, lorem ipsun, lorem lorem' })
-    @IsString()
-    @IsNotEmpty()
-    description: string;
+  @ApiProperty({
+    type: String,
+    description: 'Descrição do projeto',
+    example: 'Lorem ipsum, lorem ipsun, lorem lorem',
+  })
+  @IsString()
+  @IsNotEmpty()
+  description: string;
 
-    @ApiProperty({type: String, description: 'Link para doações', example: 'https://seila.com'})
-    @IsString()
-    @IsNotEmpty()
-    donate: string;
+  @ApiProperty({
+    type: String,
+    description: 'Link para doações',
+    example: 'https://seila.com',
+  })
+  @IsString()
+  @IsNotEmpty()
+  donate: string;
 
-    @ApiProperty({ type: String, description: 'Se o projeto é online, híbrido ou presencial', example: 'Presencial' })
-    @IsNotEmpty()
-    workType: WorkType;
+  @ApiProperty({
+    type: String,
+    description: 'Se o projeto é online, híbrido ou presencial',
+    example: 'Presencial',
+  })
+  @IsNotEmpty()
+  workType: WorkType;
 }
